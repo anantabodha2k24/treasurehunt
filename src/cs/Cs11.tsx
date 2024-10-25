808095ddd9ee89f8b520b77907ee586e4afc1eb4
like-a-hacker <goethamramesh199@gmail.com>
2024-10-25 08:36:10 +0530

    some changes

--- src/cs/Cs11.tsx
+++ src/cs/Cs11.tsx
@@ -6,10 +6,10 @@ const Csoth24 = () => {
         <div className="flex  justify-center items-center h-screen">
           <div className="flex flex-col h-max w-4/6 justify-center text-black backdrop-blur shadow-2xx rounded border-2 border-black">
             <h1 className="text-center text-xl  h-1/5 pt-2">
-              CyberSecurity
-            </h1>
+              Final
+            </h1><br/>
             <div className="text-center flex justify-center h-4/5 pt-2 py-2 flex-wrap">
-              10. Guess the Word:
+              Guess the Word:
               <img src={ph} alt="" className="w-72"/>
             </div>
           </div>
